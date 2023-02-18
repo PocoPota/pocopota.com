28041a0ddcb5d128e83d410b88f795cabae5aecc
PocoPota <pocopota.program@gmail.com>
2023-02-19 08:48:23 +0900

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
-            <Script strategy="afterInteractive" data-website-id="9ec491db-3f45-46d6-98a1-1d357d8b651a" src="https://umami.pocopota.com/umami.js" data-do-not-track="true" />
+            <Script data-website-id="9ec491db-3f45-46d6-98a1-1d357d8b651a" src="https://umami.pocopota.com/umami.js" data-do-not-track="true" strategy="afterInteractive" />
             <Component {...pageProps} />
         </>
     )
